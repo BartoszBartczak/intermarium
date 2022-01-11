ee5ca44e25dcd41f7799dd379464926ce7e833ee
BartoszBartczak <intermarism@gmail.com>
2022-01-11 14:00:11 +0100

    articles works

--- src/app/views/show/show.component.ts
+++ src/app/views/show/show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { articlesMock } from "src/app/artics";
 import { Article } from "src/app/artics.model";
 
@@ -9,12 +10,13 @@ import { Article } from "src/app/artics.model";
   styleUrls: ['./show.component.scss']
 })
 export class ShowComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   article: Article = articlesMock[0];
 
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.article = articlesMock[parseInt(params.id)];
+    });
+  }
 }
