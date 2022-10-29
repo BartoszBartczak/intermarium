87bcdd962e5fedbc8f7861bd5ccdb9bb3aaa2a9a
BartoszBartczak <intermarism@gmail.com>
2022-10-29 17:52:54 +0200

    Another changes on navbar

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  Kontakt:boolean = true
 
   constructor() { }
+
+  clickEvent(){
+    this.Kontakt=!this.Kontakt;
+    
+  }
+  
 
   ngOnInit(): void {
   }
