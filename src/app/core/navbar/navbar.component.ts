0cdd491db75e1d0de1b4c013958d94a13f8133c7
BartoszBartczak <intermarism@gmail.com>
2022-11-11 13:08:03 +0100

    Searching engine

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormArray, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Observable,} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,16 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   Kontakt:boolean = true
+  //searchForm: any;
 
-  constructor() { }
+  constructor() {  }
 
   clickEvent(){
     this.Kontakt=!this.Kontakt;
     
   }
+
+  @Input() set query(query: string | null) {
+    const field = this.searchForm.get('query')
+    if (!(field instanceof FormControl)) return
+    field.setValue(query, {})
+  }
+
+  badword = 'Putin';
+  censor = (control: AbstractControl): ValidationErrors | null => {
+
+    if (String(control.value).includes(this.badword))
+      return {
+        censor: { 'badword': this.badword }
+      }
+    
+    return null
+  }
+  asyncCensor = (control: AbstractControl): Observable<ValidationErrors | null> => {
+    
+
+    return new Observable((observer) => {
+      
+
+      const handle = setTimeout(() => {
+        const result = (String(control.value).includes(this.badword)) ? {
+          censor: { 'badword': this.badword }
+        } : null;
+        
+        observer.next(result)
+        observer.complete()
+      }, 500)
+
+      return () => {
+        
+        clearTimeout(handle)
+      }
+    })
+    
+  }
+
+  searchForm = new FormGroup({
+    'query': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      // this.censor
+    ], [
+      this.asyncCensor
+    ]),
+    'extras': new FormGroup({
+      'type': new FormControl('słowo'),
+      'markets': new FormArray([
+        new FormGroup({
+          'code': new FormControl('PL')
+        })
+      ])
+    })
+  })
   
 
   ngOnInit(): void {
+    const queryField = this.searchForm.get('query')!
+
+    const valueChanges = queryField.valueChanges
+    const statusChanges = queryField.statusChanges
+
   }
 
 }
